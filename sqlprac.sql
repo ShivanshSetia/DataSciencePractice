752587c13ff0420e5895b5f8db72beaf18bffd3c
ShivanshSetia <shivansh.setia11@gmail.com>
2023-06-12 01:23:13 -0400

    sqlprac

--- sqlprac.sql
+++ sqlprac.sql
@@ -9,3 +9,32 @@ select cast(ceiling(avg(cast(salary as float)) - avg(cast(replace(salary, '0', '
 from employees
 
 
+/*
+We define an employee's total earnings to be their monthly  worked, and the maximum
+ total earnings to be the maximum total earnings for any employee in the Employee table.
+  Write a query to find the maximum total earnings for all employees as well as the total number
+   of employees who have maximum total earnings. Then print these values as  space-separated integers.
+*/
+
+select max(months*salary), count(*)
+from employee
+where months*salary = (select max(months*salary) from employee );
+
+
+/*
+Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) 
+in STATION that is less than . Round your answer to  decimal places.
+*/
+
+
+select cast(long_w as decimal(10,4)) from station 
+where  lat_n = (select max(lat_n) from station where lat_n <137.2345  );
+
+
+/*
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than . Round your answer to  decimal places.
+*/
+
+
+select cast(min(lat_n) as decimal(10,4)) from station 
+where lat_n > 38.7780;
