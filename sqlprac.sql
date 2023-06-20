9b9f39073c2a9b4e1c52911954630d72ed72b6e2
ShivanshSetia <shivansh.setia11@gmail.com>
2023-06-20 01:25:31 -0400

    sql practice joins

--- sqlprac.sql
+++ sqlprac.sql
@@ -174,3 +174,33 @@ GROUP BY S.HACKER_ID , H.NAME
 HAVING COUNT(S.CHALLENGE_ID) > 1
 
 ORDER BY COUNT(S.CHALLENGE_ID) DESC,  S.HACKER_ID ASC;
+
+
+
+/*
+Harry Potter and his friends are at Ollivander's with Ron, finally replacing Charlie's old broken wand.
+
+Hermione decides the best way to choose is by determining the minimum number
+ of gold galleons needed to buy each non-evil wand of high power and age. 
+ Write a query to print the id, age, coins_needed, and power of the wands that Ron's 
+ interested in, sorted in order of descending power. If more than one wand has same power, 
+ sort the result in order of descending age.
+*/
+
+
+select  w.id, wp.age,w.coins_needed, w.power
+from wands w
+join 
+wands_property wp
+on wp.code = w.code
+where  wp.is_evil = 0 and  w.coins_needed = 
+ ( 
+select min(w1.coins_needed) 
+from wands w1
+join wands_property wp1 
+on wp1.code = w1.code 
+where 
+ w.power = w1.power and wp.age = wp1.age
+) 
+
+order by w.power desc , wp.age desc;
