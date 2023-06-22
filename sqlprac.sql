e3e0b612f6fb86cf9ae26727ca9149a450ef461d
ShivanshSetia <shivansh.setia11@gmail.com>
2023-06-22 00:10:52 -0400

    sql intermediate practice

--- sqlprac.sql
+++ sqlprac.sql
@@ -227,3 +227,29 @@ ORDER BY num DESC, h.hacker_id
 
 SELECT * FROM SOLUTION 
 WHERE num = (SELECT MAX(num) FROM SOLUTION) OR num in (SELECT num FROM SOLUTION GROUP BY num HAVING COUNT(num) = 1) ;
+
+
+
+
+
+/*
+You did such a great job helping Julia with her last coding contest challenge that she wants you to work on this one, too!
+The total score of a hacker is the sum of their maximum scores for all of the challenges.
+ Write a query to print the hacker_id, name, and total score of the hackers ordered by the descending score.
+  If more than one hacker achieved the same total score, then sort the result by ascending hacker_id.
+   Exclude all hackers with a total score of 0 from your result.
+*/
+
+
+SELECT h.hacker_id,h.name,sum(s.score) as total_score 
+from hackers h
+join 
+(
+select hacker_id, max(score) as score
+from submissions
+group by hacker_id,challenge_id
+) s
+on h.hacker_id = s.hacker_id
+group by h.hacker_id,h.name
+having sum(score) > 0
+order by sum(s.score) desc,h.hacker_id;
